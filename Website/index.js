9effe0d6bd6ae39eb2d74c344444cbe68fa22b72
anuragkumart16 <anuragkumartiwari12@gmail.com>
2023-04-16 08:17:57 +0530

    added the pyton and wesbite folder

---
+++ Website/index.js
@@ -0,0 +1,35 @@
+// javascript variables
+// containers to store data values is called variable
+let number1=23
+let number2=43
+console.log("here comes the output")
+console.log(number1 + number2)
+
+// datatypes in javascript
+let string1="this is string"
+let string2="this is another string"
+console.log(string1,string2)
+
+// objects (key value pairs) of javascript
+var marks={
+    ravi: 34,
+    anurag :64,
+    lucky: 92
+
+} 
+console.log(marks)
+
+// booleans
+var a= true;
+var b= false;
+
+console.log(a,b)
+
+// undefined
+var c;
+console.log(c)
+// above statement will return "undefined" as the vlue of the var is not given
+
+var d=undefined;
+console.log(d)
+// the above statement will also give undefined
